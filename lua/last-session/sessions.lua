2b4f58168e83c346d3d21aadae0004b6a84c233b
Jaehaks <wogkr3654@naver.com>
2025-04-19 23:50:12 +0900

    fix(sessions): refactoring of load_session
    It had too many nested conditional statement. fixed it

--- lua/last-session/sessions.lua
+++ lua/last-session/sessions.lua
@@ -56,29 +56,33 @@ M.load_session = function()
 	local options = config.get_config()
 	local session_file = options.path
 
-	if vim.fn.filereadable(session_file) == 1 then
-		local lines = vim.fn.readfile(session_file)
-		if lines and #lines > 0 then
-			local ok, session_data = pcall(vim.json.decode, lines[1])
-			if not ok then
-				vim.api.nvim_echo({{'Error: Invalid session file format'}}, false, {err = true})
-			end
-
-			for _, file_data in ipairs(session_data) do
-				if vim.fn.filereadable(file_data.path) == 1 then
-					-- edit only focused file to fast load
-					if file_data.focused == 1 then
-						vim.cmd('edit ' .. vim.fn.fnameescape(file_data.path))
-					else
-						vim.cmd('badd ' .. vim.fn.fnameescape(file_data.path))
-					end
-				end
-			end
-		else
-			vim.api.nvim_echo({{'Error: Could not read session file'}}, false, {err = true})
-		end
-	else
+	-- check if session file is exists
+	if vim.fn.filereadable(session_file) ~= 1 then
 		vim.api.nvim_echo({{'No session file found at ' .. session_file}}, false, {err = true})
+		return
+	end
+
+	-- check if session file contents is empty
+	local lines = vim.fn.readfile(session_file)
+	if not lines and #lines == 0 then
+		vim.api.nvim_echo({{'Error: Could not read session file'}}, false, {err = true})
+		return
+	end
+
+	-- decode session file contents
+	local ok, session_data = pcall(vim.json.decode, lines[1])
+	if not ok then
+		vim.api.nvim_echo({{'Error: Invalid session file format'}}, false, {err = true})
+		return
+	end
+
+	-- load buffers
+	for _, file_data in ipairs(session_data) do
+		if vim.fn.filereadable(file_data.path) == 1 then
+			-- edit only focused file to fast load
+			local cmd_open = file_data.focused == 1 and 'edit' or 'badd'
+			vim.cmd(cmd_open .. vim.fn.fnameescape(file_data.path))
+		end
 	end
 end
 
